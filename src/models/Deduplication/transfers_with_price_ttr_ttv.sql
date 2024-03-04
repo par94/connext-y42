44a3a2813173c6aaf3f4f98aad628a9d76a3114c
Anton Piskunov <anton@connext.network>
2024-03-04 16:45:18 -0700

    dedup_transfers_with_price

--- src/models/Deduplication/transfers_with_price_ttr_ttv.sql
+++ src/models/Deduplication/transfers_with_price_ttr_ttv.sql
@@ -1,5 +1,7 @@
 SELECT
      *,
-     row_number() OVER (PARTITION BY `id` ORDER BY SystemModstamp DESC) AS row_num
-FROM {{ source('Cartographer', 'public_daily_transfer_volume') }}
+     tf.execute_timestamp - tf.xcall_timestamp AS ttv,
+     tf.reconcile_timestamp - tf.xcall_timestamp AS ttr,
+     row_number() OVER (PARTITION BY `transfer_id` ORDER BY tf.update_time DESC) AS row_num
+FROM {{ source('Cartographer', 'public_transfers_with_price') }} AS tf
 QUALIFY row_num = 1
