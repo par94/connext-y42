c426f885ddcbf89278883acf4273f695b9c9167e
Anton Piskunov <anton@connext.network>
2024-03-04 17:32:15 -0700

    c

--- src/models/Views/daily_transfer_volume_y42_view.sql
+++ src/models/Views/daily_transfer_volume_y42_view.sql
@@ -4,7 +4,7 @@ SELECT
     tf.destination_domain AS destination_chain,
     tf.origin_transacting_asset AS asset,
     DATE_TRUNC(CAST(TIMESTAMP_SECONDS(tf.xcall_timestamp) AS DATE), DAY) AS transfer_date,
-    REGEXP_REPLACE(tf.routers, r'[\{\}]', '') AS router
+    REGEXP_REPLACE(tf.routers, r'[\{\}]', '') AS router,
     SUM(CAST(tf.origin_transacting_amount AS NUMERIC)) AS volume,
     AVG(tf.asset_usd_price) AS avg_price,
     SUM(tf.usd_amount) AS usd_volume,
