8276e538410425aebb70c3432910ed1c525ac086
Anton Piskunov <anton@connext.network>
2024-03-11 19:28:59 -0700

    c

--- src/models/Transformations/slippage_historical1.sql
+++ src/models/Transformations/slippage_historical1.sql
@@ -1,16 +1,18 @@
 {{ config(materialized='incremental') }}
 
-{% set source_count = dbt_utils.get_row_count(ref{{'slippage_historical'}}) %}
+{{ ref('daily_transfer_volume_y42_dedup') }}
+
+{% set source_count = dbt_utils.get_row_count({{ ref('daily_transfer_volume_y42_dedup') }}) %}
 {% set model_count = dbt_utils.get_row_count(this) %}
 
 {% if source_count > model_count %}
     -- If the source table has more rows, perform a complete copy
     SELECT *
-    FROM ref{{'slippage_historical'}})
+    FROM {{ ref('daily_transfer_volume_y42_dedup') }}
 {% else %}
     -- If the source table has the same or fewer rows, perform an incremental update
     SELECT *
-    FROM ref{{'slippage_historical'}})
+    FROM {{ ref('daily_transfer_volume_y42_dedup') }}
     WHERE row_hash NOT IN (
         SELECT row_hash
         FROM {{ this }}
