f23777ff3f416e0ef8ad15df7023ca9fde36d625
Anton Piskunov <anton@connext.network>
2024-03-11 20:01:00 -0700

    c

--- src/models/Transformations/slippage_historical1.sql
+++ src/models/Transformations/slippage_historical1.sql
@@ -1,6 +1,10 @@
-   SELECT *
-    FROM {{ ref('slippage_historical') }}
-    WHERE row_hash NOT IN (
-        SELECT row_hash
-        FROM {{ this }}
-    )
+{{ config(materialized='incremental', unique_key='row_hash', incremental_strategy='append') }}   
+SELECT *
+FROM {{ ref('slippage_historical') }}
+
+{% if is_incremental() %}
+
+    -- this filter will only be applied on an incremental run
+    WHERE row_hash NOT IN (SELECT (row_hash) FROM {{ this }})
+
+{% endif %}
