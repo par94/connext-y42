a71559f8bd23b9750fbbb102a8471a5577be50e3
Anton Piskunov <anton@connext.network>
2024-03-11 17:48:49 -0700

    c

--- src/models/Transformations/slippage_historical.sql
+++ src/models/Transformations/slippage_historical.sql
@@ -1,2 +1,8 @@
---select * from {{ this }} union all
+--with cte as (
+ --   select * from {{ this }}
+---)
+--select * from cte
+
+select * from {{ ref('slippage_historical') }}
+union all
 select * from {{ source('slippage_monitoring', 'slippage_monitoring') }}
