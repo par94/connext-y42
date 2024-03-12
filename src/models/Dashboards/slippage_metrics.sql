048f09857f992eac6b2020fdd934ccb7ebc660a5
Anton Piskunov <anton@connext.network>
2024-03-11 20:26:49 -0700

    c

--- src/models/Dashboards/slippage_metrics.sql
+++ src/models/Dashboards/slippage_metrics.sql
@@ -2,17 +2,17 @@ SELECT
     slippage,
     CAST(amount as BIGINT) / power(10, 18) AS amount_eth,
     CASE
-        WHEN domain = '6648936' THEN 'Ethereum'
-        WHEN domain = '1869640809' THEN 'Optimism'
-        WHEN domain = '6450786' THEN 'BNB'
-        WHEN domain = '6778479' THEN 'Gnosis'
-        WHEN domain = '1886350457' THEN 'Polygon'
-        WHEN domain = '1634886255' THEN 'Arbitrum One'
-        WHEN domain = '1818848877' THEN 'Linea'
-        WHEN domain = '1835365481' THEN 'Metis'
-        WHEN domain = '1650553709' THEN 'Base'
+        WHEN domain_id = '6648936' THEN 'Ethereum'
+        WHEN domain_id = '1869640809' THEN 'Optimism'
+        WHEN domain_id = '6450786' THEN 'BNB'
+        WHEN domain_id = '6778479' THEN 'Gnosis'
+        WHEN domain_id = '1886350457' THEN 'Polygon'
+        WHEN domain_id = '1634886255' THEN 'Arbitrum One'
+        WHEN domain_id = '1818848877' THEN 'Linea'
+        WHEN domain_id = '1835365481' THEN 'Metis'
+        WHEN domain_id = '1650553709' THEN 'Base'
         ELSE
-            domain
+            domain_id
     END
         AS chain_name,
     format_timestamp('%Y-%m-%d %H:%M:%S', timestamp_seconds(timestamp)) AS timestamp_time
