88169457749ac27451318436a4ec73cbd6e44746
Anton Piskunov <anton@connext.network>
2024-03-11 18:14:51 -0700

    c

--- src/models/Transformations/slippage_historical.sql
+++ src/models/Transformations/slippage_historical.sql
@@ -1,8 +1,10 @@
-{{ config(materialized='incremental') }}
+{{ config(materialized='incremental', unique_key='row_hash') }}
 
 WITH source_data AS (
-    SELECT *
+    SELECT *,
     --CURRENT_TIMESTAMP() AS version_timestamp -- Captures the refresh timestamp
+    MD5(CONCAT(',', COALESCE(CAST(domain_id AS STRING), ''), COALESCE(CAST(asset AS STRING), ''), 
+    COALESCE(CAST(amount AS STRING), ''), COALESCE(CAST(`timestamp` AS STRING), ''))) AS row_hash
     FROM {{ source('slippage_monitoring', 'slippage_monitoring') }}
 )
 
