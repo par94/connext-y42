526659d0fadbee933ba075352226364292a24e25
Anton Piskunov <anton@connext.network>
2024-03-26 23:33:19 -0700

    c

--- src/models/Dashboards/router_metrics.sql
+++ src/models/Dashboards/router_metrics.sql
@@ -47,7 +47,7 @@ WITH VolumeMetrics AS (
       1,2,3,4,5 
 ),
 RouterMapping AS (
-  SELECT * 
+  SELECT *,
   FROM VolumeMetrics tm
   LEFT JOIN {{ ref('routers_with_balances_mapped') }} rm
     ON tm.`destination_domain` = rm.`domain`
