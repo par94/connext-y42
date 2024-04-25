ec9451bf1238561ce8a80c5ac3e270a224634290
Anton Piskunov <anton@connext.network>
2024-04-24 20:28:46 -0500

    c

--- src/models/Deduplication/daily_transfer_volume_y42_dedup.sql
+++ src/models/Deduplication/daily_transfer_volume_y42_dedup.sql
@@ -2,4 +2,4 @@ SELECT
     *,
     row_number() OVER (PARTITION BY `id` ORDER BY `volume` DESC) AS ROW_NUM
 FROM {{ source('Cartographer', 'public_daily_transfer_volume') }}
-QUALIFY ROW_NUM = 1
+WHERE TRUE QUALIFY ROW_NUM = 1
